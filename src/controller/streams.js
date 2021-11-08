559ef9227b283102fbda6d22d3ea6cd8eb77d031
rahultripathi <rahutripathi@ewar.in>
2021-11-08 17:29:32 +0530

    - updated stream controllers

--- src/controller/streams.js
+++ src/controller/streams.js
@@ -3,16 +3,43 @@ const {
   modelFields: { streamFields },
   models
 } = require('../models');
+const helpers = {
+  swap: (arr, indexA, indexB) => {
+    [arr[indexA], arr[indexB]] = [arr[indexB], arr[indexA]];
+  },
+  findPartitionIndex: (array, start, end, key) => {
+    let pivot = array[end][key];
+    let partitionIndex = start;
+    for (let i = start; i < end; i++) {
+      if (array[i][key] < pivot) {
+        helpers.swap(array, i, partitionIndex);
+        partitionIndex++;
+      }
+    }
+    helpers.swap(array, end, partitionIndex);
+    return partitionIndex;
+  },
+  quickSortObjects: (array, start, end, key) => {
+    if (start < end) {
+      let partitionIndex = helpers.findPartitionIndex(array, start, end, key);
+      helpers.quickSortObjects(array, partitionIndex + 1, end, key);
+      helpers.quickSortObjects(array, start, partitionIndex - 1, key);
+    }
+  }
+};
 
 const getAllStreams = (req, res, next) => {
   let { order = 'DESC' } = req.query;
   models.streams
     .findAll({
-      order: [[streamFields.viewer_count, order]],
+      order: [[streamFields.viewer_count, 'DESC']],
       limit: 100,
       raw: true
     })
     .then(streams => {
+      if (order == 'ASC') {
+        helpers.quickSortObjects(streams, 0, streams.length - 1, streamFields.viewer_count);
+      }
       res.send(formatResponse(200, streams, 'fetched all streams'));
     })
     .catch(err => {
